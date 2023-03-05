6e31e492e221d5de2ebc9180bcbf1172987fdee6
michał <michal.maziarz12@gmail.com>
2023-03-05 20:19:20 +0100

    Double linked list impl and tests

--- lista_1/zadanie_1/doubleLinkedList.cpp
+++ lista_1/zadanie_1/doubleLinkedList.cpp
@@ -18,61 +18,173 @@ public:
 };
 
 template <class T>
-void printList(Node<T> *firstNode);
+void print(Node<T> *headNode);
 
 template <class T>
-Node<T> *search(Node<T> *firstNode, int index);
+void append(Node<T> **headRef, T data);
 
 template <class T>
-void addAnyNode(Node<T> *firstNode, Node<T> *newNode, int index);
+void push(Node<T> **headRef, T data);
 
 template <class T>
-void removeNode(Node<T> *firstNode, int index);
+void insertAfter(Node<T> *node, T data);
 
-void testPrintList();
+template <class T>
+void deleteNode(Node<T> *node);
 
-void testSearch();
+void appendTest();
 
-void testAddAnyNode();
+void pushTest();
 
-void testRemoveNode();
+void insertAfterTest();
+
+void deleteTest();
+
+void assert(bool test, int number);
 
 int main()
 {
+    appendTest();
+    pushTest();
+    insertAfterTest();
+    deleteTest();
+
     return 0;
 }
 
 template <class T>
-void printList(Node<T> *firstNode)
+void print(Node<T> *headNode)
 {
-    Node<T> *nodePtr = firstNode;
+    Node<T> *nodePtr = headNode;
     while (nodePtr)
     {
         cout << nodePtr->data << " ";
         nodePtr = nodePtr->next;
     }
+    cout << endl;
 }
 
 template <class T>
-Node<T> *search(Node<T> *firstNode, int index)
+void append(Node<T> **headRef, T data)
 {
-    Node<T> *nodePtr = firstNode;
-    for (int i = 0; i < index; i++)
+    Node<T> *newNode = new Node<T>(data);
+    Node<T> *nodePtr = *headRef;
+    if (*headRef == NULL)
+    {
+        *headRef = newNode;
+        return;
+    }
+    while (nodePtr->next != nullptr)
     {
         nodePtr = nodePtr->next;
-        if (nodePtr == nullptr)
-        {
-            throw invalid_argument("Array index out of bound");
-        }
     }
+    nodePtr->next = newNode;
+    newNode->previous = nodePtr;
 }
 
-void testPrintList()
+template <class T>
+void push(Node<T> **headRef, T data)
 {
-    Node<string> *n1 = new Node<string>("ciesc");
-    Node<string> *n2 = new Node<string>("kolega");
-    Node<string> *n3 = new Node<string>("witam");
-    Node<string> *n4 = new Node<string>("cie");
+    Node<T> *newNode = new Node(data);
+
+    if (*headRef != nullptr)
+    {
+        (*headRef)->previous = newNode;
+        newNode->next = *headRef;
+    }
+    *headRef = newNode;
+}
+
+template <class T>
+void insertAfter(Node<T> *node, T data)
+{
+    auto *newNode = new Node(data);
+    if (node->next == nullptr)
+    {
+        node->next = newNode;
+        newNode->previous = node;
+        return;
+    }
+    newNode->next = node->next;
+    newNode->previous = node;
+    node->next->previous = newNode;
+    node->next = newNode;
+}
+
+template <class T>
+void deleteNode(Node<T> *node)
+{
+    if (node->next != nullptr && node->previous != nullptr)
+    {
+        node->next->previous = node->previous;
+        node->previous->next = node->next;
+    }
+    else if (node->next == nullptr)
+    {
+        node->previous->next = nullptr;
+    }
+    else if (node->previous == nullptr)
+    {
+        node->next->previous = nullptr;
+    }
+    delete node;
+}
+
+void appendTest()
+{
+    cout << "Double linked list push to end test" << endl;
+    Node<int> *headNode = nullptr;
+    append(&headNode, 0);
+    append(&headNode, 1);
+    bool test1 = headNode->data == 0;
+    bool test2 = headNode->next->data == 1;
+    assert(test1, 1);
+    assert(test2, 2);
+}
+
+void pushTest()
+{
+    cout << "Push to begin of Double linked list test" << endl;
+    Node<int> *head = nullptr;
+    push(&head, 0);
+    bool test1 = head->data == 0;
+    push(&head, 1);
+    bool test2 = head->data == 1 && head->next->data == 0;
+    print(head);
+    assert(test1, 1);
+    assert(test2, 2);
+}
+
+void insertAfterTest()
+{
+    cout << "Insert after list test. should be 0 1 2 3 4 AND 4 3 2 1 0" << endl;
+    Node<int> *head = new Node<int>(0);
+    Node<int> *n1 = new Node<int>(1);
+    Node<int> *n3 = new Node<int>(3);
+    head->next = n1;
+    n1->previous = head;
+    n1->next = n3;
+    n3->previous = n1;
+    insertAfter(n1, 2);
+    insertAfter(n3, 4);
+    print(head);
+    Node<int> *ptr = n3->next;
+    while (ptr != nullptr)
+    {
+        cout << ptr->data << " ";
+        ptr = ptr->previous;
+    }
+    cout << endl;
+}
+
+void deleteTest()
+{
+    cout << "Deletion of node in list test" << endl;
+
+    Node<int> *n1 = new Node<int>(1);
+    Node<int> *n2 = new Node<int>(2);
+    Node<int> *n3 = new Node<int>(3);
+    Node<int> *n4 = new Node<int>(4);
 
     n1->next = n2;
     n2->previous = n1;
@@ -81,5 +193,23 @@ void testPrintList()
     n3->next = n4;
     n4->previous = n3;
 
-    printList(n1);
+    deleteNode(n2);
+    print(n1);
+    deleteNode(n4);
+    print(n1);
+    deleteNode(n1);
+    print(n3);
+}
+
+void assert(bool test, int number)
+{
+    cout << "\tTest " << number;
+    if (test)
+    {
+        cout << " passed\n";
+    }
+    else
+    {
+        cout << " failed\n";
+    }
 }
